47b2d83750d5f553b55737bd3aeaeb53bfaff65a
WebGCDev <gaetano.chi@gmail.com>
2023-11-09 19:36:33 +0100

    feat: Loading Ok prova estrazione numeri con una funzione HumanVsAi

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -4,3 +4,9 @@ const min = 1;
 const max = 6;
 
 let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+playerHuman = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(playerHuman);
+
+playerAi = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(playerAi);
