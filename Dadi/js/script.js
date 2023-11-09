9dbc3275d09da4f2fb1d7bb470563798bac5e171
WebGCDev <gaetano.chi@gmail.com>
2023-11-09 20:02:07 +0100

    feat:Esercizio 2 Completo Loading ok All result Ok

--- Dadi/js/script.js
+++ Dadi/js/script.js
@@ -10,3 +10,11 @@ console.log(playerHuman);
 
 playerAi = Math.floor(Math.random() * (max - min + 1)) + min;
 console.log(playerAi);
+
+if (playerHuman === playerAi) {
+  output.innerHTML = 'PAREGGIO';
+} else if (playerHuman < playerAi) {
+  output.innerHTML = 'Computer with Ai VINCE';
+} else {
+  output.innerHTML = 'PlayerHuman VINCE e si festeggia';
+}
