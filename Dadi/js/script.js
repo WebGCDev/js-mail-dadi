3eb980825160018464522140595090b1f2317546
WebGCDev <gaetano.chi@gmail.com>
2023-11-09 19:14:55 +0100

    feat : Inserimento regole del gioco dei Dadi

---
+++ Dadi/js/script.js
@@ -0,0 +1,6 @@
+let playerHuman, playerAi;
+const output = document.getElementById('output');
+const min = 1;
+const max = 6;
+
+let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
